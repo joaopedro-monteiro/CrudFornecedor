5fb158400f35e4f7344e9a4aedc144cf21db48cc
João Pedro Monteiro <joao.joaopedro@hotmail.com>
2023-08-25 17:29:41 -0300

    Criando o controller e view para o Fornecedor

---
+++ CrudFornecedor/Controllers/FornecedoresController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CrudFornecedor.Data;
+using CrudFornecedor.Models;
+
+namespace CrudFornecedor.Controllers
+{
+    public class FornecedoresController : Controller
+    {
+        private readonly Context _context;
+
+        public FornecedoresController(Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Fornecedores
+        public async Task<IActionResult> Index()
+        {
+              return _context.Fornecedor != null ? 
+                          View(await _context.Fornecedor.ToListAsync()) :
+                          Problem("Entity set 'Context.Fornecedor'  is null.");
+        }
+
+        // GET: Fornecedores/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null || _context.Fornecedor == null)
+            {
+                return NotFound();
+            }
+
+            var fornecedor = await _context.Fornecedor
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (fornecedor == null)
+            {
+                return NotFound();
+            }
+
+            return View(fornecedor);
+        }
+
+        // GET: Fornecedores/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Fornecedores/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome,Cnpj,Especialidade,Cep")] Fornecedor fornecedor)
+        {
+            if (ModelState.IsValid)
+            {
+                fornecedor.Id = Guid.NewGuid();
+                _context.Add(fornecedor);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(fornecedor);
+        }
+
+        // GET: Fornecedores/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null || _context.Fornecedor == null)
+            {
+                return NotFound();
+            }
+
+            var fornecedor = await _context.Fornecedor.FindAsync(id);
+            if (fornecedor == null)
+            {
+                return NotFound();
+            }
+            return View(fornecedor);
+        }
+
+        // POST: Fornecedores/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,Nome,Cnpj,Especialidade,Cep")] Fornecedor fornecedor)
+        {
+            if (id != fornecedor.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(fornecedor);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FornecedorExists(fornecedor.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(fornecedor);
+        }
+
+        // GET: Fornecedores/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null || _context.Fornecedor == null)
+            {
+                return NotFound();
+            }
+
+            var fornecedor = await _context.Fornecedor
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (fornecedor == null)
+            {
+                return NotFound();
+            }
+
+            return View(fornecedor);
+        }
+
+        // POST: Fornecedores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            if (_context.Fornecedor == null)
+            {
+                return Problem("Entity set 'Context.Fornecedor'  is null.");
+            }
+            var fornecedor = await _context.Fornecedor.FindAsync(id);
+            if (fornecedor != null)
+            {
+                _context.Fornecedor.Remove(fornecedor);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FornecedorExists(Guid id)
+        {
+          return (_context.Fornecedor?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ CrudFornecedor/Data/Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CrudFornecedor.Models;
+
+namespace CrudFornecedor.Data
+{
+    public class Context : DbContext
+    {
+        public Context (DbContextOptions<Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CrudFornecedor.Models.Fornecedor> Fornecedor { get; set; } = default!;
+    }
+}
