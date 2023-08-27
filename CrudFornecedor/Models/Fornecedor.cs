882ad50d15a9d95661d84c6731675d942184f936
João Pedro Monteiro <joao.joaopedro@hotmail.com>
2023-08-26 23:12:03 -0300

    Adicionando estilos e formatando alguns nomes de propriedades

--- CrudFornecedor/Models/Fornecedor.cs
+++ CrudFornecedor/Models/Fornecedor.cs
@@ -1,5 +1,6 @@
 ﻿using CrudFornecedor.Models.Enums;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace CrudFornecedor.Models;
 
@@ -12,15 +13,21 @@ public class Fornecedor
     public string? Nome { get; set; }
 
    [StringLength(18, ErrorMessage = "O CNPJ deve conter apenas 14 números")]
+    [DisplayName("CNPJ")]
     public string? Cnpj { get; set; }
     public Especialidade Especialidade { get; set; }
 
     [DataType(DataType.PostalCode)]
+    [DisplayName("CEP")]
     public string? Cep { get; set; }
+
+    [DisplayName("Rua")]
     public string? EnderecoRua { get; set; }
 
-    
+    [DisplayName("Nº")]
     public string? EnderecoNumero { get; set; }
+
+    [DisplayName("Bairro")]
     public string? EnderecoBairro { get; set; }
     public string? Cidade { get; set; }
     public string? Estado { get; set; }
